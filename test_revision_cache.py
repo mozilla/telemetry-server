03f396e46e470b821fbb3fd2dd13957fa01c29b1
Mark Reid <mreid@mozilla.com>
2013-06-13 11:10:45 -0300

    Add a basic cache for Histograms.json

---
+++ test_revision_cache.py
@@ -0,0 +1,25 @@
+import os
+import revision_cache
+import shutil
+
+cache_dir = "/tmp/histogram_revision_cache"
+assert not os.path.exists(cache_dir)
+
+def check_one(repo, rev):
+   rcache = revision_cache.RevisionCache(cache_dir, 'hg.mozilla.org')
+   filename = "%s/%s/%s/Histograms.json" % (cache_dir, repo, rev)
+   assert not os.path.exists(filename)
+   bad = rcache.fetch_disk(repo, rev)
+   assert bad == None
+   remote = rcache.fetch_server(repo, rev)
+   assert "A11Y_INSTANTIATED_FLAG" in remote
+   good = rcache.fetch_disk(repo, rev)
+   assert "A11Y_INSTANTIATED_FLAG" in good
+   assert os.path.exists(filename)
+
+check_one('mozilla-central', '26cb30a532a1')
+check_one('mozilla-aurora',  'a4de5411f118')
+check_one('mozilla-beta',    '53c447ff5fd3')
+check_one('mozilla-release', '0488055e9f9f')
+
+shutil.rmtree(cache_dir)
