58ac57376c1ed62b8beb9de7ecd240b83f266ad8
Mark Reid <mreid@mozilla.com>
2013-07-02 17:23:52 -0300

    Basic MapReduce working

--- job.py
+++ job.py
@@ -7,9 +7,11 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
 
 import argparse
+import imp
 import sys
 import os
 import json
+from multiprocessing import Process, Queue
 from telemetry_schema import TelemetrySchema
 
 class Job:
@@ -30,25 +32,60 @@ class Job:
         self._output_file = output_file
         self._num_mappers = mappers
         self._num_reducers = reducers
+        modulefd = open(job_script)
+        ## Lifted from FileDriver.py in jydoop.
+        self._job_module = imp.load_module("telemetry_job", modulefd, job_script, ('.py', 'U', 1))
+
+    def mapreduce(self):
+        files = self.files()
+        partitions = self.partition(files)
+        map_output_files = Queue()
+        #print "Found partitions", partitions
+        mappers = []
+        for i in range(self._num_mappers):
+            p = Process(
+                    target=Mapper,
+                    args=(map_output_files, "mapper_%d"%i, partitions[i], self._job_module))
+            mappers.append(p)
+            p.start()
+        for m in mappers:
+            m.join()
+
+        # Mappers are done.  Reduce.
+        reducers = []
+        for i in range(self._num_reducers):
+            p = Process(
+                    target=Reducer,
+                    args=(map_output_files, "reducer_%d"%i, self._job_module))
+            reducers.append(p)
+            p.start()
+        for r in reducers:
+            r.join()
+
+        # Reducers are done.  Output results.
 
     def files(self):
         out_files = self.get_filtered_files(self._input_dir)
         if self._input_filter._include_invalid:
             invalid_dir = os.path.join(self._input_dir, TelemetrySchema.INVALID_DIR)
-            print "Looking for invalid data in", invalid_dir
+            #print "Looking for invalid data in", invalid_dir
             out_files += self.get_filtered_files(invalid_dir)
         return out_files
 
     # Split up the input files into groups of approximately-equal on-disk size.
     def partition(self, files):
-        namesize = [ { "name": files[i], "size": os.stat(files[i]).st_size } for i in range(0, len(files)) ]
-        partitions = [[]] * self._num_mappers
-        sums = [0] * self._num_mappers
+        namesize = [ { "name": files[i], "size": os.stat(files[i]).st_size, "dimensions": self._input_filter.get_dimensions(self._input_dir, files[i]) } for i in range(0, len(files)) ]
+        partitions = []
+        sums = []
+        for p in range(self._num_mappers):
+            partitions.append([])
+            sums.append(0)
         min_idx = 0
 
         # Greedily assign the largest file to the smallest partition
         while len(namesize) > 0:
             current = namesize.pop()
+            #print "putting", current, "into partition", min_idx
             partitions[min_idx].append(current)
             sums[min_idx] += current["size"]
             for m in range(0, len(sums)):
@@ -81,6 +118,60 @@ class Job:
         allowed_values = self._allowed_values[level]
         return self._input_filter.is_allowed(value, allowed_values)
 
+class Context:
+    def __init__(self, out):
+        self._sink = open(out, "w")
+
+    def write(self, key, value):
+        self._sink.write(str(key))
+        self._sink.write("\t")
+        self._sink.write(str(value))
+        self._sink.write("\n")
+
+    def finish(self):
+        self._sink.close()
+
+class Mapper:
+    def __init__(self, output_files, name, inputs, module):
+        #print "I am", name, ", and I'm mapping", len(inputs), "inputs:", inputs
+        output_file = "output/" + name
+        mapfunc = getattr(module, 'map', None)
+        context = Context(output_file)
+        if mapfunc is None or not callable(mapfunc):
+            print "No map function!!!"
+            sys.exit(1)
+        for input_file in inputs:
+            input_fd = open(input_file["name"], "r")
+            for line in input_fd:
+                key, value = line.split("\t", 1)
+                mapfunc(key, input_file["dimensions"], value, context)
+        context.finish()
+        output_files.put(output_file)
+
+class Reducer:
+    def __init__(self, mapper_files, name, module):
+        print "I am", name, ", and I'm reducing", mapper_files.qsize(), "mapped files"
+        output_file = "output/" + name
+        context = Context(output_file)
+        reducefunc = getattr(module, 'reduce', None)
+        if reducefunc is None or not callable(reducefunc):
+            print "No reduce function (that's ok)"
+        else:
+            collected = {}
+            while not mapper_files.empty():
+                mapper_file = mapper_files.get(False)
+                # read, group by key, call reducefunc, output
+                input_fd = open(mapper_file, "r")
+                for line in input_fd:
+                    key, value = line.split("\t", 1)
+                    if key not in collected:
+                        collected[key] = []
+                    collected[key].append(value)
+            for k,v in collected.iteritems():
+                reducefunc(k, v, context)
+        context.finish()
+
+
 def main(argv=None):
     parser = argparse.ArgumentParser(description='Run a MapReduce Job.')
     parser.add_argument("job_script", help="The MapReduce script to run")
@@ -94,11 +185,11 @@ def main(argv=None):
     job = Job(args.data_dir, args.output,args.job_script, args.num_mappers, args.num_reducers, args.input_filter)
 
     files = job.files()
-    print "Found ", len(files), "files:"
+    print "Processing", len(files), "input files"
     #print files
 
-    print job.partition(files)
-
+    job.mapreduce()
+    print "All done"
 
 if __name__ == "__main__":
     sys.exit(main())
