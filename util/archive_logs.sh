9337697b694763727245da4e4242c37ad1c3f4c9
Mark Reid <mreid@mozilla.com>
2013-08-01 15:22:21 -0300

    Send archive output to a log file

--- util/archive_logs.sh
+++ util/archive_logs.sh
@@ -2,6 +2,8 @@
 
 CODE_DIR=$1
 DATA_DIR=$2
+LOG_FILE=/tmp/telemetry_archive.log
+
 USAGE="Usage: $0 code_dir data_dir"
 if [ -z "$CODE_DIR" ] || [ ! -f "$CODE_DIR/get_compressibles.py" ]; then
   echo "Invalid code_dir"
@@ -19,6 +21,6 @@ cd $CODE_DIR
 # Note that this assumes a bunch of stuff, including that all the defaults
 # for compression will work relative to $CODE_DIR (histogram_cache location,
 # server config, etc)
-$PYTHON ./get_compressibles.py $DATA_DIR | $PYTHON ./compressor.py
+time $PYTHON ./get_compressibles.py $DATA_DIR | $PYTHON ./compressor.py >> $LOG_FILE 2>&1
 
 exit $?
