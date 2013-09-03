c0c8843330bdc2cfec5f34207aed94c276d0ce02
Mark Reid <mreid@mozilla.com>
2013-09-03 18:01:32 -0300

    Switch things to Classes

--- process_inline.py
+++ process_inline.py
@@ -28,7 +28,6 @@ from boto.s3.connection import S3Connection
 import util.timer as timer
 import struct, gzip, StringIO
 
-
 def fetch_s3_files(files, fetch_cwd, bucket_name, aws_key, aws_secret_key):
     result = 0
     if len(files) > 0:
@@ -52,63 +51,70 @@ def fetch_s3_files(files, fetch_cwd, bucket_name, aws_key, aws_secret_key):
         print "Downloaded %.2fMB in %.2fs (%.2fMB/s)" % (downloaded_mb, duration_sec, downloaded_mb / duration_sec)
     return result
 
-def abort_on_error(err):
-    # TODO: make this kill all procs
-    while True:
-        try:
-            error = err.get(True, 3)
-            if is_sentinel(error):
-                break
-            #if isinstance(error, basestring):
-            sys.stderr.write(str(error) + "\n")
-            #else:
-            #    sys.stderr.write(json.dumps(error))
-            #sys.exit(1)
-        except Q.Empty:
-            #print "No errors so far!"
-            pass
-    
 def wait_for(processes, label):
     print "Waiting for", label, "..."
     for p in processes:
         p.join()
     print label, "Done."
 
-def is_sentinel(value):
-    return value is None
+class PipeStep(object):
+    PAUSE_LENGTH = 2
+    RETRIES = 3
+    def __init__(self, num, name, q_in, q_out=None):
+        self.num = num
+        self.label = " ".join((name, str(num)))
+        self.q_in = q_in
+        self.q_out = q_out
+        self.records_read = 0
+        self.records_written = 0
 
-def get_sentinel():
-    return None
+        # Do stuff.
+        self.setup()
+        self.work()
+        self.finish()
+    def setup(self):
+        pass
+    def finish(self):
+        print self.label, "All done, read", self.records_read, "records, wrote", self.records_written, "records"
+        pass
+    def handle(self, record):
+        pass
+    def work(self):
+        print self.label, "Starting up"
+        retries = PipeStep.RETRIES
+        while True:
+            try:
+                raw = self.q_in.get(True, PipeStep.PAUSE_LENGTH)
+                self.handle(raw)
+                self.records_read += 1
+            except Q.Empty:
+                if retries > 0:
+                    print self.label, "- Get timed out, trying again"
+                    retries -= 1
+                else:
+                    break
 
-def read_raw_records(n, num_cpus, raw_files, schema, q_raw, errors):
-    print "Raw Reader #", n, "of", num_cpus, "starting up"
+class ReadRawStep(PipeStep):
+    def __init__(self, num, name, raw_files, q_raw, schema):
+        self.schema = schema
+        PipeStep.__init__(self, num, name, raw_files, q_raw)
 
-    #time.sleep(10)
-    #errors.put("woops")
+    def setup(self):
+        self.expected_dim_count = len(self.schema._dimensions)
 
-    expected_dim_count = len(schema._dimensions)
-    while True:
-        # Wait (potentially forever) for a raw file
-        raw_file = raw_files.get()
-        if is_sentinel(raw_file):
-            print "reader", n, "got exit message"
-            # put it back for other processes.
-            #raw_files.put('exit')
-            # Pass it along to the next stage too.
-            q_raw.put(get_sentinel())
-            break
-        print "Reader", n, "reading", raw_file
+    def handle(self, raw_file):
+        print self.label, "reading", raw_file
         try:
             fin = open(raw_file, "rb")
             bytes_read = 0
             record_count = 0
             start = datetime.now()
             while True:
-                record_count += 1
                 # Read two 4-byte values and one 8-byte value
                 lengths = fin.read(16)
                 if lengths == '':
                     break
+                record_count += 1
                 len_path, len_data, timestamp = struct.unpack("<IIQ", lengths)
 
                 # Incoming timestamps are in milliseconds, so convert to POSIX first
@@ -131,21 +137,21 @@ def read_raw_records(n, num_cpus, raw_files, schema, q_raw, errors):
                         data_reader.close()
                     except Exception, e:
                         # Corrupted data, let's skip this record.
-                        print "Warning: Found corrupted data for record", record_count, "in", raw_file, "path:", path
+                        print self.label, "Warning: Found corrupted data for record", record_count, "in", raw_file, "path:", path
                         continue
                 elif data[0] != "{":
                     # Data looks weird, should be JSON.
-                    print "Warning: Found unexpected data for record", record_count, "in", raw_file, "path:", path, "data:"
+                    print self.label, "Warning: Found unexpected data for record", record_count, "in", raw_file, "path:", path, "data:"
                     print data
 
                 bytes_read += 8 + len_path + len_data
                 #print "Path for record", record_count, path, "length of data:", len_data, "data:", data[0:5] + "..."
 
                 path_components = path.split("/")
-                if len(path_components) != expected_dim_count:
+                if len(path_components) != self.expected_dim_count:
                     # We're going to pop the ID off, but we'll also add the submission,
                     # so it evens out.
-                    print "Found an invalid path in record", record_count, path
+                    print self.label, "Found an invalid path in record", record_count, path
                     continue
 
                 key = path_components.pop(0)
@@ -155,61 +161,36 @@ def read_raw_records(n, num_cpus, raw_files, schema, q_raw, errors):
                 info["appVersion"] = path_components.pop(0)
                 info["appUpdateChannel"] = path_components.pop(0)
                 info["appBuildID"] = path_components.pop(0)
-                dimensions = schema.dimensions_from(info, submission_date)
-                q_raw.put((key, dimensions, data))
+                dimensions = self.schema.dimensions_from(info, submission_date)
+                self.q_out.put((key, dimensions, data))
+                self.records_written += 1
             duration = timer.delta_sec(start)
             mb_read = bytes_read / 1024.0 / 1024.0
-            print "Reader", n, "- Read %.2fMB in %.2fs (%.2fMB/s)" % (mb_read, duration, mb_read / duration)
+            print self.label, "- Read %d records %.2fMB in %.2fs (%.2fMB/s)" % (record_count, mb_read, duration, mb_read / duration)
         except Exception, e:
             # Corrupted data, let's skip this record.
-            errors.put({"err": e, "message": "Error reading raw data from " + raw_file, "id": n})
-            continue
-    print "Reader", n, "all done"
-    sys.exit(0)
+            print self.label, "- Error reading raw data from ", raw_file, e
 
-def convert_raw_records(n, num_cpus, q_raw, q_converted, q_bad, errors):
-    print "Converter #", n, "of", num_cpus, "starting up"
-    #time.sleep(10)
-    #errors.put("woops")
-    while True:
-        raw = q_raw.get()
-        if is_sentinel(raw):
-            print "converter", n, "got exit message"
-            # tell the other processes to exit too
-            q_raw.put(get_sentinel())
-            # pass it along and clean up
-            q_converted.put(get_sentinel())
-            break
-        key, dims, data = raw
-        #print "Converter", n, "got", key
-    print "Converter", n, "all done"
-    sys.exit(0)
 
-def write_converted_records(n, num_cpus, q_converted, q_completed, errors):
-    print "Writer #", n, "of", num_cpus, "starting up"
+class WriteConvertedStep(PipeStep):
+    def handle(self, record):
+        print self.label, "got a record"
 
-    while True:
-        converted = q_converted.get()
-        if is_sentinel(converted):
-            print "writer", n, "got exit message"
-            q_converted.put(get_sentinel())
-            # Note: do NOT pass 'exit' forward to q_completed, since we'll
-            #       need to process all the non-rotated files when we are
-            #       all finished coverting.
-            break
-    print "Writer", n, "all done"
-    sys.exit(0)
 
-def export_completed_files(n, num_cpus, q_completed, errors):
-    print "Exporter #", n, "of", num_cpus, "starting up"
+class ExportCompletedStep(PipeStep):
+    def handle(self, record):
+        print self.label, "got a record"
 
-    time.sleep(10)
-    errors.put("exporter %d woops" % (n))
-    sys.exit(0)
 
-def add_sentinels(queue, count):
-    for i in range(count):
-        queue.put(get_sentinel())
+class ConvertRawRecordsStep(PipeStep):
+    def __init__(self, num, name, q_in, q_out, q_bad):
+        self.q_bad = q_bad
+        PipeStep.__init__(self, num, name, q_in, q_out)
+
+    def handle(self, record):
+        key, dims, data = record
+        print self.label, "got", key
+
 
 def main():
     parser = argparse.ArgumentParser(description='Process incoming Telemetry data', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -263,7 +244,6 @@ def main():
     raw_files = Queue(100)
     for l in local_filenames:
         raw_files.put(l)
-    add_sentinels(raw_files, num_cpus)
 
     raw_records = Queue(150000)
     converted_records = Queue(50000)
@@ -271,19 +251,12 @@ def main():
     completed_files = Queue()
     compressed_files = Queue()
 
-    # Fatal processing errors.
-    errors = Queue()
-
-    error_watcher = Process(target=abort_on_error, args=(errors))
-    error_watcher.start()
-    print "error_watcher pid:", error_watcher.pid
-
     # Begin reading raw input
     raw_readers = []
     for i in range(num_cpus):
         rr = Process(
-                target=read_raw_records,
-                args=(i, num_cpus, raw_files, schema, raw_records, errors))
+                target=ReadRawStep,
+                args=(i, "Reader", raw_files, raw_records, schema))
         raw_readers.append(rr)
         rr.start()
         print "Reader", i, "pid:", rr.pid
@@ -293,8 +266,8 @@ def main():
     converters = []
     for i in range(num_cpus):
         cr = Process(
-                target=convert_raw_records,
-                args=(i, num_cpus, raw_records, converted_records, bad_records, errors))
+                target=ConvertRawRecordsStep,
+                args=(i, "Converter", raw_records, converted_records, bad_records))
         converters.append(cr)
         cr.start()
         print "Converter", i, "pid:", cr.pid
@@ -304,8 +277,8 @@ def main():
     writers = []
     for i in range(num_cpus):
         w = Process(
-                target=write_converted_records,
-                args=(i, num_cpus, converted_records, completed_files, errors))
+                target=WriteConvertedStep,
+                args=(i, "Writer", converted_records, completed_files))
         writers.append(w)
         w.start()
         print "Writer", i, "pid:", w.pid
@@ -315,8 +288,8 @@ def main():
     exporters = []
     for i in range(num_cpus):
         e = Process(
-                target=export_completed_files,
-                args=(i, num_cpus, completed_files, errors))
+                target=ExportCompletedStep,
+                args=(i, "Exporter", completed_files))
         exporters.append(e)
         e.start()
         print "Exporter", i, "pid:", e.pid
@@ -343,11 +316,6 @@ def main():
 
     duration = timer.delta_sec(start)
     print "All done in %.2fs" % (duration)
-
-    errors.put(get_sentinel())
-
-    # We haven't had an error by now, terminate the error watcher.
-    error_watcher.terminate()
     return 0
 
 if __name__ == "__main__":
