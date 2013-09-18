3bdcfaad3d824df95a593dd9d387a729b9df37d5
Mark Reid <mreid@mozilla.com>
2013-09-18 13:10:04 -0300

    Fix imports for conversion utility

--- util/convert_log_v0_to_v1.py
+++ util/convert_log_v0_to_v1.py
@@ -2,8 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import sys, struct, gzip, time
-import StringIO as StringIO
+import sys, struct
 
 fin = open(sys.argv[1], "rb")
 fout = open(sys.argv[2], "wb")
