a0f7f675f066d2c51cf39f9f8e33b48b330600e5
Mark Reid <mreid@mozilla.com>
2013-10-18 12:40:47 -0300

    Implement uploading and deleting.

--- process_incoming_combine.py
+++ process_incoming_combine.py
@@ -27,38 +27,48 @@ from revision_cache import RevisionCache
 from persist import StorageLayout
 import boto.sqs
 import traceback
+from boto.s3.key import Key
 
+
+class UploadError(Exception):
+    pass
+class CompressError(Exception):
+    pass
+class DecompressError(Exception):
+    pass
 
 class Combiner:
     """A class for combining small Telemetry files into larger ones in S3"""
     MAX_ACTIVITIES = 10
 
-    def __init__(self, log, dry_run):
+    def __init__(self, log, dry_run, keep_remote_files, keep_local_files):
         self.log = log
         self.dry_run = dry_run
+        self.keep_remote_files = keep_remote_files
+        self.keep_local_files = keep_local_files
 
     def log_activity(self, activity):
         with io.open(self.log, "a") as fout:                                    
             fout.write(json.dumps(activity, separators=(u',', u':')) + u"\n")
 
-    def recover(self, source_bucket, dest_bucket):
+    def recover(self, conn):
         # Read through the log's activities
         print "Reading log", self.log
         if not os.path.exists(self.log):
             print "No log file present. Recovery not needed."
             return True
 
-        last_publishables = None
-        last_deletables = None
+        last_pub_activity = None
+        last_del_activity = None
         log_file = open(self.log, "r")
         recent_activities = []
         # TODO: handle json-parsing exceptions
         for line in log_file.readlines():
             activity = json.loads(line)
             if activity["activity"] == "PUBLISH":
-                last_publishables = activity["files"]
+                last_pub_activity = activity
             if activity["activity"] == "DELETE" or activity["activity"] == "COMBINE":
-                last_deletables = activity["files"]
+                last_del_activity = activity
             recent_activities.append(activity)
             if len(recent_activities) > Combiner.MAX_ACTIVITIES:
                 recent_activities.pop(0)
@@ -91,22 +101,27 @@ class Combiner:
         if do_publish:
             print "Cleaning up by publishing (again?)"
             # Get publishables:
-            if last_publishables is None:
+            if last_pub_activity is None:
                 print "Cannot recover... missing required list of files to be published"
                 # TODO: throw error
                 success = False
             else:
-                self.upload(last_publishables, dest_bucket)
+                dest_bucket = conn.get_bucket(last_pub_activity["bucket"])
+                dest_dir = last_pub_activity["base_dir"]
+                self.upload(last_pub_activity["files"], dest_bucket, dest_dir)
+                self.local_delete(last_pub_activity["files"], dest_dir)
 
         if do_delete:
             print "Cleaning up by deleting (again?)"
-            if last_deletables is None:
-                # Look for a recent "COMBINE" for the list.
+            if last_del_activity is None:
                 print "Cannot recover... missing required list of files to be deleted"
                 # TODO: throw error
                 success = False
             else:
-                self.delete(last_deletables, source_bucket)
+                source_bucket = conn.get_bucket(last_del_activity["bucket"])
+                source_dir = last_del_activity["base_dir"]
+                self.delete(last_del_activity["files"], source_bucket, source_dir)
+                self.local_delete(last_del_activity["files"], source_dir)
         return success
 
     def fetch_file_info(self, bucket, max_size, prefix=''):
@@ -136,7 +151,14 @@ class Combiner:
         self.log_activity(activity)
         for f in files:
             print "Getting", f.name
+            # Check locally first (md5) and skip downloading files we already
+            # have.
             local_filename = os.path.join(base_dir, f.name)
+            if os.path.exists(local_filename):
+                md5, size = fileutil.md5file(local_filename)
+                if md5 == f.etag[1:-1]:
+                    print "Already have", f.name
+                    continue
             local_dir = os.path.dirname(local_filename)
             if not os.path.exists(local_dir):
                 os.makedirs(local_dir)
@@ -160,8 +182,7 @@ class Combiner:
             full_name = os.path.join(base_dir, f.name)
             result = subprocess.call(decompress_cmd + [full_name], stdout=tmp_file)
             if result != 0:
-                print "ERROR"
-                # TODO: throw
+                raise DecompressError("Unexpected decompress return code: " + str(result))
         tmp_file.close()
 
         # Get md5sum of tmp_name (raw uncompressed contents)
@@ -173,8 +194,14 @@ class Combiner:
         print "Renaming", tmp_name, "to", large_file
         full_large_file = os.path.join(out_dir, large_file)
         os.rename(tmp_name, full_large_file)
+        # If the compressed file already exists, delete it first.
+        if os.path.exists(full_large_file + StorageLayout.COMPRESSED_SUFFIX):
+            os.remove(full_large_file + StorageLayout.COMPRESSED_SUFFIX)
+
         compress_cmd = [StorageLayout.COMPRESS_PATH] + StorageLayout.COMPRESSION_ARGS + [full_large_file]
         result = subprocess.call(compress_cmd)
+        if result != 0:
+            raise CompressError("Unexpected compress return code: " + str(result))
 
         activity["state"] = "FINISH"
         self.log_activity(activity)
@@ -184,7 +211,10 @@ class Combiner:
         larges = []
         size = 0
         current = []
-        activity = {"state": "START", "activity": "COMBINE", "files": [ s.name for s in smalls] }
+        bucket_name = smalls[0].bucket.name
+        activity = {"state": "START", "activity": "COMBINE",
+                "files": [ s.name for s in smalls], "bucket": bucket_name,
+                "base_dir": work_dir}
         self.log_activity(activity)
         for s in smalls:
             size += s.size
@@ -196,7 +226,7 @@ class Combiner:
         if len(current) > 1:
             larges.append(current)
 
-        # TODO: actually concatenate the files
+        # Now download and concatenate the files
         large_map = {}
         for to_be_combined in larges:
             self.download(to_be_combined, work_dir)
@@ -207,30 +237,75 @@ class Combiner:
         self.log_activity(activity)
         return large_map
 
-    def upload(self, larges, bucket):
-        # TODO: implement
-        activity = {"state": "START", "activity": "PUBLISH", "files": larges}
+    def upload(self, larges, bucket, base_dir):
+        activity = {"state": "START", "activity": "PUBLISH", "files": larges,
+                "base_dir": base_dir, "bucket": bucket.name}
         self.log_activity(activity)
         for large in larges:
             print "Uploading", large
-        if self.dry_run:
-            print "Dry run: not really uploading"
-        else:
-            print "TODO: upload for reals"
+            if self.dry_run:
+                print "Dry run: not really uploading"
+            else:
+                # TODO: wrap in retry.
+                k = Key(bucket)
+                k.key = large
+                full_large = os.path.join(base_dir, large)
+                bytes_written = k.set_contents_from_filename(full_large)
+                actual_bytes = os.path.getsize(full_large)
+                if bytes_written != actual_bytes:
+                    message = "Error uploading %s - bytes written: %d, actual bytes: %d" % (full_large, bytes_written, actual_bytes)
+                    print message
+                    raise UploadError(message)
         activity["state"] = "FINISH"
         self.log_activity(activity)
 
-    def delete(self, smalls, bucket):
-        # TODO
-        activity = {"state": "START", "activity": "DELETE", "files": smalls}
+    # Delete files from S3. Not to be confused with "local_delete" which
+    # deletes files on local disk
+    def delete(self, smalls, bucket, base_dir):
+        activity = {"state": "START", "activity": "DELETE", "files": smalls,
+                "base_dir": base_dir, "bucket": bucket.name}
         self.log_activity(activity)
         print "Deleting", smalls
         if self.dry_run:
             print "Dry run: not really deleting"
+        elif self.keep_remote_files:
+            print "Not deleting remote files (received --keep-remote-files arg)"
         else:
-            print "TODO: delete for reals"
-            # result = bucket.delete_keys(smalls)
-            # iterate result to make sure they all got deleted.
+            # S3 multi-deletes can do 1000 at a time. The boto docs don't say
+            # whether this is abstracted by the library, so we split the list
+            # "just in case".
+            delete_groups = [ smalls[i:i+1000] for i in range(0, len(smalls), 1000) ]
+            for delete_group in delete_groups:
+                print "Deleting", len(delete_group), "keys"
+                # TODO: wrap in retry
+                result = bucket.delete_keys(delete_group)
+                # Note: An attempt to delete a non-existent file does not cause
+                #       an error.
+                # Check result to make sure they all got deleted.
+                if len(result.deleted) < len(delete_group):
+                    print "Not everything was deleted successfully"
+                    for e in result.errors:
+                        print "Not deleted:", e
+                    raise DeleteError("Not Deleted:" + ",".join(result.errors))
+                else:
+                    print "Successfully deleted", len(result.deleted), "keys"
+        activity["state"] = "FINISH"
+        self.log_activity(activity)
+
+    def local_delete(self, files, base_dir):
+        activity = {"state": "START", "activity": "LOCAL_DELETE",
+                "files": files, "base_dir": base_dir}
+        self.log_activity(activity)
+
+        print "Locally deleting", files
+        if self.dry_run:
+            print "Dry run: not really deleting"
+        elif self.keep_local_files:
+            print "Not deleting local files (received --keep-local-files arg)"
+        else:
+            for f in files:
+                print "Deleting", f
+                os.remove(os.path.join(base_dir, f))
         activity["state"] = "FINISH"
         self.log_activity(activity)
 
@@ -245,6 +320,8 @@ def main():
     parser.add_argument("-o", "--output-dir", help="Base dir to store processed data", required=True)
     parser.add_argument("-p", "--prefix", help="Optional prefix to limit input files", default='')
     parser.add_argument("-m", "--max-output-size", metavar="N", help="Max combined file size", type=int, default=(100 * 1024 * 1024))
+    parser.add_argument("--keep-remote-files", help="Don't remove small files from S3", action="store_true")
+    parser.add_argument("--keep-local-files", help="Don't remove local files from disk", action="store_true")
     parser.add_argument("-D", "--dry-run", help="Don't modify remote files", action="store_true")
     args = parser.parse_args()
 
@@ -271,9 +348,10 @@ def main():
     activity_log_file = os.path.join(args.work_dir, "activity.log")
 
     start = datetime.now()
-    combiner = Combiner(activity_log_file, args.dry_run)
+    combiner = Combiner(activity_log_file, args.dry_run,
+            args.keep_remote_files, args.keep_local_files)
 
-    if combiner.recover(source_bucket, dest_bucket):
+    if combiner.recover(conn):
         print "Recovery succeeded!"
         if args.dry_run:
             print "If this wasn't a dry run, we'd be deleting the log file after a successful recovery"
@@ -281,6 +359,8 @@ def main():
             os.remove(activity_log_file)
     else:
         print "Recovery failed... aborting"
+        # TODO: email the (compressed) activity log, upload it to S3, or
+        #       otherwise complain loudly somehow.
         return 1
 
     combinees = combiner.fetch_file_info(source_bucket, args.max_output_size, args.prefix)
@@ -290,11 +370,13 @@ def main():
         large_map = combiner.combine(partition, smalls, args.max_output_size, args.work_dir, args.output_dir)
         larges = sorted(large_map.keys())
         print "Uploading", len(larges), "files to replace", len(smalls), "small ones."
-        combiner.upload(larges, dest_bucket)
+        combiner.upload(larges, dest_bucket, args.output_dir)
+        combiner.local_delete(larges, args.output_dir)
 
         print "Deleting", len(smalls), "small files."
         small_names = [ s.name for s in smalls ]
-        combiner.delete(small_names, args.source_bucket)
+        combiner.delete(small_names, source_bucket, args.work_dir)
+        combiner.local_delete(small_names, args.work_dir)
 
     duration = timer.delta_sec(start)
     print "All done in %.2fs" % (duration)
