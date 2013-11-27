0872c28d8c7a0ff9c28a39b68f97aa91fa9ed259
Mark Reid <mreid@mozilla.com>
2013-11-27 11:30:44 -0400

    Add Upload support to s3 utils
    Rename 'Downloader' -> 'Loader', and make it do uploads as well as downloads.

--- telemetry/util/s3.py
+++ telemetry/util/s3.py
@@ -4,16 +4,17 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from boto.s3.connection import S3Connection
-from boto.s3.key import Key
 from multiprocessing import Pool
 import multiprocessing
 import os
 import sys
 from traceback import print_exc
+import telemetry.util.files as fu
+from boto.exception import S3ResponseError
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
 
-
-class Downloader:
+class Loader:
     def __init__(self, local_path, bucket_name, aws_key=None, aws_secret_key=None, poolsize=10, verbose=False):
         self.verbose = verbose
         self.local_path = local_path
@@ -24,51 +25,41 @@ class Downloader:
         self.bucket = self.conn.get_bucket(self.bucket_name)
         self.poolsize=poolsize
 
-    def progress(self):
-        p_len = len(self.pending)
-        if p_len == 0:
-            return 100
-        d_len = len(self.downloaded)
-        if d_len == 0:
-            return 0
-        return int(d_len / (p_len + d_len))
-
     def make_args(self, files):
         for f in files:
-            yield [self.local_path, self.bucket, f]
+            yield [self.local_path, self.bucket, f.name if type(f) == Key else f]
 
-    def make_args_from_key(self, keys):
-        for f in keys:
-            yield [self.local_path, self.bucket, f.name]
-
-    def fetch_list(self, files):
+    def load_list(self, files, load_function):
         pool = Pool(processes=self.poolsize)
-        result_iterator = pool.imap_unordered(download_one, self.make_args(files))
+        result_iterator = pool.imap_unordered(load_function, self.make_args(files))
         while True:
             try:
                 yield result_iterator.next(timeout=1)
             except multiprocessing.TimeoutError:
-                print "no results yet.."
+                if self.verbose:
+                    print "no results yet.."
             except StopIteration:
                 break
 
-    def fetch_schema(self, schema):
-        pool = Pool(processes=self.poolsize)
-        result_iterator = pool.imap_unordered(download_one, self.make_args_from_key(list_partitions(self.bucket, schema=schema, include_keys=True)))
-        while True:
-            try:
-                yield result_iterator.next(timeout=1)
-            except multiprocessing.TimeoutError:
-                print "no results yet.."
-            except StopIteration:
-                break
+    def get_list(self, files):
+        for local_filename, remote_filename, err in self.load_list(files, download_one):
+            yield local_filename, remote_filename, err
+
+    def get_schema(self, schema):
+        for local_filename, remote_filename, err in self.load_list(list_partitions(self.bucket, schema=schema, include_keys=True), download_one):
+            yield local_filename, remote_filename, err
+
+    def put_list(self, files):
+        for local_filename, remote_filename, err in self.load_list(files, upload_one):
+            yield local_filename, remote_filename, err
+
 
 def download_one(args):
     local_path, bucket, remote_key = args
     target = os.path.join(local_path, remote_key)
     target_dir = os.path.dirname(target)
     if not os.path.exists(target_dir):
-        os.makedirs(target_dir)
+        fu.makedirs_concurrent(target_dir)
     success = False
     err = None
     for retry in range(1, 4):
@@ -80,13 +71,40 @@ def download_one(args):
             #       multipart uploads.
             success = True
             break
+        except S3ResponseError, e:
+            print >> sys.stderr, "S3 Error on attempt #%i:" % retry, e.status, e.reason
         except:
             print >> sys.stderr, "Error on attempt #%i:" % retry
             print_exc(file = sys.stderr)
     if not success:
         err = "Failed to download '%s' as '%s'" % (remote_key, target)
         print >> sys.stderr, err
-    return target, err
+    return target, remote_key, err
+
+def upload_one(args):
+    local_path, bucket, remote_key = args
+    target = os.path.join(local_path, remote_key)
+    success = False
+    err = None
+    for retry in range(1, 4):
+        try:
+            k = Key(bucket)
+            k.key = remote_key
+            # TODO: use multipart upload for large files
+            k.set_contents_from_filename(target)
+            # TODO: compare md5?
+            success = True
+            break
+        except S3ResponseError, e:
+            print >> sys.stderr, "S3 Error on attempt #%i:" % retry, e.status, e.reason
+        except:
+            print >> sys.stderr, "Error on attempt #%i:" % retry
+            print_exc(file = sys.stderr)
+    if not success:
+        err = "Failed to upload '%s' as '%s'" % (target, remote_key)
+        print >> sys.stderr, err
+    return target, remote_key, err
+
 
 def list_partitions(bucket, prefix='', level=0, schema=None, include_keys=False):
     #print "Listing...", prefix, level
