610d50363eccaa14948fad9e01c98d9ac8241717
Mark Reid <mreid@mozilla.com>
2014-02-05 15:45:40 -0400

    Add files for running the flash export via cron.

---
+++ mapreduce/flash/crontab.spec
@@ -0,0 +1,2 @@
+# m h  dom mon dow   command
+  5 16  *   *   *    /home/ubuntu/run_flash_export.sh
---
+++ mapreduce/flash/run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+OUTPUT=output
+TODAY=$(date +%Y%m%d)
+if [ ! -d "$OUTPUT" ]; then
+    mkdir -p "$OUTPUT"
+fi
+
+if [ ! -d "job" ]; then
+    mkdir -p "job"
+fi
+if [ ! -d "work" ]; then
+    mkdir -p "work"
+fi
+if [ ! -d "data" ]; then
+    mkdir -p "data"
+fi
+
+# If we have an argument, process that day.
+TARGET=$1
+if [ -z "$TARGET" ]; then
+  # Default to processing "yesterday"
+  TARGET=$(date -d 'yesterday' +%Y%m%d)
+fi
+
+echo "Today is $TODAY, and we're gathering flash versions for $TARGET"
+sed -r "s/__TARGET_DATE__/$TARGET/" filter_template.json > filter_flash.json
+
+BASE=$(pwd)
+cd ~/telemetry-server
+echo "Starting the flash versions export for $TARGET"
+python -u -m mapreduce.job $BASE/flash_versions.py \
+  --num-mappers 16 \
+  --input-filter $BASE/filter_flash.json \
+  --data-dir $BASE/data \
+  --work-dir $BASE/work \
+  --output $BASE/$OUTPUT/flash_versions$TARGET.csv.tmp \
+  --bucket telemetry-published-v1
+
+echo "Mapreduce job exited with code: $?"
+
+cd -
+echo "Looking for 'error' lines:"
+grep -e "^Error," $OUTPUT/flash_versions$TARGET.csv.tmp
+echo "End of error lines."
+
+echo "Adding header line and removing error lines..."
+cp csv_header.txt $OUTPUT/flash_versions$TARGET.csv
+grep -ve "^Error," $OUTPUT/flash_versions$TARGET.csv.tmp >> $OUTPUT/flash_versions$TARGET.csv
+echo "Removing temp file"
+rm $OUTPUT/flash_versions$TARGET.csv.tmp
+echo "Compressing output"
+gzip $OUTPUT/flash_versions$TARGET.csv
+echo "Done!"
