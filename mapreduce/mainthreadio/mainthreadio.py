0964ae50ec201885ff37e830168da3d8b55a3645
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2014-02-12 11:41:33 +0000

    Merge Main-Thread IO job

---
+++ mapreduce/mainthreadio/mainthreadio.py
@@ -0,0 +1,69 @@
+import simplejson as json
+import numpy
+import io
+import csv
+from string import maketrans
+
+def clean(s):
+    return normalize(s).translate(None, ",")
+
+def normalize(s):
+    if type(s) == unicode:
+        return s.encode('utf8', 'ignore')
+    else:
+        return str(s)
+
+def safe_key(pieces):
+    output = io.BytesIO()
+    writer = csv.writer(output, quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(pieces)
+    return output.getvalue().strip()
+
+def map(k, d, v, cx):
+    global n_pings
+
+    parsed = json.loads(v)
+    reason, appName, appUpdateChannel, appVersion, appBuildID, submission_date = d
+
+    if not "fileIOReports" in parsed:
+        return
+
+    if not parsed["fileIOReports"]:
+        return
+
+    cx.write(safe_key([submission_date, appName, appVersion, appUpdateChannel, "TOTAL"]), [0, 0, 0, 0, 0, 0])
+
+    for f, arr in parsed["fileIOReports"].iteritems():
+        cx.write(safe_key([submission_date, appName, appVersion, appUpdateChannel, clean(f)]), arr)
+
+def setup_reduce(cx):
+    cx.field_separator = ","
+
+def reduce(k, v, cx):
+    totals = []
+    avgs = []
+    counts = []
+    n_opens = []
+    n_reads = []
+    n_writes = []
+    n_fsyncs = []
+    n_stats = []
+    n_pings = 0
+
+    for total, n_open, n_read, n_write, n_fsync, n_stat in v:
+        totals.append(total)
+        n_opens.append(n_open)
+        n_reads.append(n_read)
+        n_writes.append(n_write)
+        n_fsyncs.append(n_fsync)
+        n_stats.append(n_stat)
+        n_pings += 1
+
+        count = n_open + n_read + n_write + n_fsync + n_stat
+        counts.append(count)
+
+    if n_pings > 100:
+        # Output fields:
+        # submission_date, app_name, app_version
+        # app_update_channel, filename, submission_count, median_time, median_count
+        cx.write(k, ",".join([str(n_pings), str(numpy.median(totals)), str(numpy.median(count))]))
---
+++ mapreduce/mainthreadio/run.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+sudo apt-get --yes install python-numpy
+
+OUTPUT=output
+TODAY=$(date +%Y%m%d)
+if [ ! -d "$OUTPUT" ]; then
+    mkdir -p "$OUTPUT"
+fi
+
+if [ ! -d "job" ]; then
+    mkdir -p "job"
+fi
+if [ ! -d "work" ]; then
+    mkdir -p "work"
+fi
+if [ ! -d "data" ]; then
+    mkdir -p "data"
+fi
+
+# If we have an argument, process that day.
+TARGET=$1
+if [ -z "$TARGET" ]; then
+  # Default to processing "yesterday"
+  TARGET=$(date -d 'yesterday' +%Y%m%d)
+fi
+
+echo "Today is $TODAY, and we're gathering mainthreadio data for $TARGET"
+sed -r "s/__TARGET_DATE__/$TARGET/" filter_template.json > filter.json
+
+BASE=$(pwd)
+FINAL_DATA_FILE=$BASE/$OUTPUT/mainthreadio$TARGET.csv
+RAW_DATA_FILE=${FINAL_DATA_FILE}.tmp
+cd ~/telemetry-server
+echo "Starting the mainthreadio export for $TARGET"
+python -u -m mapreduce.job $BASE/mainthreadio.py \
+  --num-mappers 16 \
+  --num-reducers 4 \
+  --input-filter $BASE/filter.json \
+  --data-dir $BASE/data \
+  --work-dir $BASE/work \
+  --output $RAW_DATA_FILE \
+  --bucket telemetry-published-v1 #--data-dir $BASE/work/cache --local-only
+
+echo "Mapreduce job exited with code: $?"
+
+echo "Adding header line"
+cp $BASE/csv_header.txt $FINAL_DATA_FILE
+cat $RAW_DATA_FILE >> $FINAL_DATA_FILE
+echo "Removing temp file"
+rm $RAW_DATA_FILE
+echo "Compressing output"
+gzip $FINAL_DATA_FILE
+echo "Done!"
+
+echo "Processing weekly data"
+cd $BASE
+if [ ! -d "weekly" ]; then
+    mkdir -p "weekly"
+fi
+cd weekly
+# Monday is day 1
+OFFSET=$(( $(date -d $TARGET +%u) - 1 ))
+MONDAY=$(date -d "$TARGET - $OFFSET days" +%Y%m%d)
+SUNDAY=$(date -d "$MONDAY + 6 days" +%Y%m%d)
+echo "For target '$TARGET', week is $MONDAY to $SUNDAY"
+for f in $(seq 0 6); do
+    DAY=$(date -d "$MONDAY + $f days" +%Y%m%d)
+    if [ "$DAY" -eq "$TARGET" ]; then
+        echo "Using local file for today ($DAY)"
+        cp $BASE/$OUTPUT/mainthreadio$DAY.csv.gz ./
+    else
+        echo "Fetching $DAY"
+	aws s3 cp s3://telemetry-public-analysis/mainthreadio/data/mainthreadio$DAY.csv.gz ./mainthreadio$DAY.csv.gz
+    fi
+done
+echo "Creating weekly data for $MONDAY to $SUNDAY"
+python $BASE/combine.py $BASE/$OUTPUT $MONDAY $SUNDAY
+echo "Created weekly output files:"
+ls -l $BASE/$OUTPUT/
