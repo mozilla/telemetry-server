8b79588bd619dc23aa8d3b89fbf7b4b4e586645a
Benjamin Smedberg <benjamin@smedbergs.us>
2014-02-21 14:54:29 -0500

    Script to check the distribution of values in a histogram.

---
+++ mapreduce/examples/distribution.py
@@ -0,0 +1,18 @@
+"""
+Get the distribution of a boolean measurement.
+"""
+
+import simplejson as json
+
+key = "NEWTAB_PAGE_SHOWN"
+
+def map(k, d, v, cx):
+    j = json.loads(v)
+    histograms = j.get("histograms", {})
+    count = 0
+    if key in histograms:
+        count = histograms[key][1]
+    cx.write(count, 1)
+
+def reduce(k, v, cx):
+    cx.write(k, sum(v))
