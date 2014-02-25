13c963c3070120333e0cfd9487118dc632d234ef
Mark Reid <mreid@mozilla.com>
2014-02-25 14:48:24 -0400

    Change default debug instance type

--- analysis/debug-service/config.py
+++ analysis/debug-service/config.py
@@ -3,7 +3,7 @@ SECRET_KEY        = 'Overwrite with a secret on deployment'
 
 # AWS EC2 configuration
 AWS_REGION        = 'us-west-2'
-INSTANCE_TYPE     = 'm2.4xlarge'
+INSTANCE_TYPE     = 'c3.2xlarge'
 SECURITY_GROUPS   = []
 INSTANCE_PROFILE  = 'telemetry-analysis-profile'
 INSTANCE_APP_TAG  = 'telemetry-analysis-debug-instance'
