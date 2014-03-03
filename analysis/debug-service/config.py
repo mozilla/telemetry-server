75da4beddffd1301a7b821240aa2a8f0c11bf1a8
Mark Reid <mreid@mozilla.com>
2014-03-03 14:41:56 -0400

    Add more S3 buckets

--- analysis/debug-service/config.py
+++ analysis/debug-service/config.py
@@ -1,13 +1,15 @@
 
-SECRET_KEY        = 'Overwrite with a secret on deployment'
+SECRET_KEY         = 'Overwrite with a secret on deployment'
 
 # AWS EC2 configuration
-AWS_REGION        = 'us-west-2'
-INSTANCE_TYPE     = 'c3.2xlarge'
-SECURITY_GROUPS   = []
-INSTANCE_PROFILE  = 'telemetry-analysis-profile'
-INSTANCE_APP_TAG  = 'telemetry-analysis-worker-instance'
-EMAIL_SOURCE      = 'jonasfj@mozilla.com'
+AWS_REGION         = 'us-west-2'
+INSTANCE_TYPE      = 'c3.2xlarge'
+SECURITY_GROUPS    = []
+INSTANCE_PROFILE   = 'telemetry-analysis-profile'
+INSTANCE_APP_TAG   = 'telemetry-analysis-worker-instance'
+EMAIL_SOURCE       = 'jonasfj@mozilla.com'
 
-# Bucket for storing public ssh-keys
-TEMPORARY_BUCKET  = 'bucket-for-ssh-keys'
+# Buckets for storing S3 data
+TEMPORARY_BUCKET   = 'bucket-for-ssh-keys'
+CODE_BUCKET        = 'bucket-for-job-code'
+PUBLIC_DATA_BUCKET = 'bucket-for-job-data'
