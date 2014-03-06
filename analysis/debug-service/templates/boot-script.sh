5ade9050c471c7017bf20a4c2322bf99f7eda228
Mark Reid <mreid@mozilla.com>
2014-03-06 10:54:37 -0400

    Clone the telemetry-server repo and add a MOTD
    This makes it easier to get started with an analysis worker node.

--- analysis/debug-service/templates/boot-script.sh
+++ analysis/debug-service/templates/boot-script.sh
@@ -3,7 +3,7 @@
 cd /home/ubuntu
 
 # Install a few dependencies
-sudo apt-get -y install xz-utils python-pip
+sudo apt-get -y install xz-utils python-pip git
 sudo pip install --upgrade boto awscli
 
 # Get users ssh key
@@ -18,3 +18,24 @@ END
 # Setup users ssh_key
 cat /home/ubuntu/user_key.pub >> /home/ubuntu/.ssh/authorized_keys
 chmod 600 /home/ubuntu/.ssh/authorized_keys
+
+# Setup the motd
+sudo cat >/etc/motd <<END
+Welcome to a Telemetry Analysis worker node.
+
+* For a quick intro, see:
+  http://mreid-moz.github.io/blog/2013/11/06/current-state-of-telemetry-analysis/
+
+* The telemetry-server repository is at ~/telemetry-server
+
+* Don't forget to copy any important code and data off this machine! It will
+  only be available for 24 hours, after which its contents will be...
+          ********************
+          *** GONE FOREVER ***
+          ********************
+
+* If you get stuck, drop by #telemetry on irc.mozilla.org
+END
+
+# Check out telemetry-server repo:
+sudo -u ubuntu git clone https://github.com/mozilla/telemetry-server.git
