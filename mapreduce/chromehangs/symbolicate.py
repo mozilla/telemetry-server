2e3e35064bf3a6456f40efd99f9f56b0e9d57bae
Mark Reid <mreid@mozilla.com>
2014-06-04 09:53:18 -0300

    Add the MPL blurb

--- mapreduce/chromehangs/symbolicate.py
+++ mapreduce/chromehangs/symbolicate.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""
-symbolicate.py
-Copyright (c) 2012 Mozilla Foundation. All rights reserved.
-"""
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import sys
 import getopt
