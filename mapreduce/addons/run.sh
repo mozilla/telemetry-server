aff8a09b824f748edcb152df6dd95164f1ddda27
Irving Reid <irving@mozilla.com>
2014-06-10 11:04:24 -0400

    Switch to branch after cloning.

--- mapreduce/addons/run.sh
+++ mapreduce/addons/run.sh
@@ -6,6 +6,8 @@ sudo pip install unicodecsv
 # Replace the default telemetry-server install with our own
 rm -rf telemetry-server
 git clone https://github.com/irvingreid/telemetry-server.git
+cd telemetry-server
+git checkout addon-nightly
 
 # Now run the actually processing job, using the code from Irving's github
-telemetry-server/mapreduce/addons/processExceptions.sh
+mapreduce/addons/processExceptions.sh
