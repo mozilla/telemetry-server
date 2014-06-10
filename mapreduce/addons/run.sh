58f8249d9757793cdc2c089377c44f86befedb97
Irving Reid <irving@mozilla.com>
2014-06-10 11:06:29 -0400

    processExceptions.sh expects to run from root dir

--- mapreduce/addons/run.sh
+++ mapreduce/addons/run.sh
@@ -6,8 +6,7 @@ sudo pip install unicodecsv
 # Replace the default telemetry-server install with our own
 rm -rf telemetry-server
 git clone https://github.com/irvingreid/telemetry-server.git
-cd telemetry-server
-git checkout addon-nightly
+(cd telemetry-server; git checkout addon-nightly)
 
 # Now run the actually processing job, using the code from Irving's github
-mapreduce/addons/processExceptions.sh
+telemetry-server/mapreduce/addons/processExceptions.sh
