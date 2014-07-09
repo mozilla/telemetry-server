a5df6c2d4e0c19b53a2914ba3d9bff1af33aba56
Mark Reid <mreid@mozilla.com>
2014-07-09 13:19:12 -0300

    Add MPL blurb

--- monitoring/sanitize_fxos/sanitize_fxos_pings.py
+++ monitoring/sanitize_fxos/sanitize_fxos_pings.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from boto.s3.connection import S3Connection
 from datetime import datetime
 import argparse
