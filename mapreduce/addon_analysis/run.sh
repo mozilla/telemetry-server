71dabf01b77a14e71be8cc63b28041e0168b96df
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2014-10-20 12:27:16 +0100

    Update run.sh

--- mapreduce/addon_analysis/run.sh
+++ mapreduce/addon_analysis/run.sh
@@ -5,9 +5,9 @@ sudo add-apt-repository --yes ppa:marutter/rrutter
 sudo apt-get update
 sudo apt-get --yes install python-scipy python-numpy git r-base r-base-dev pcregrep
 
-#rm -rf telemetry-server
-#git clone https://github.com/mozilla/telemetry-server.git
-#cd telemetry-server/mapreduce/addon_analysis
+rm -rf telemetry-server
+git clone https://github.com/mozilla/telemetry-server.git
+cd telemetry-server/mapreduce/addon_analysis
 
 OUTPUT=output
 TODAY=$(date +%Y%m%d)
