4c4f1cb29f36de48db5f9f154c57c07231915824
Mark Reid <mreid@mozilla.com>
2015-03-17 14:42:57 -0300

    Tweak 'jq' usage and fix quoting issues.

--- http/analysis-service/jobs/run.sh
+++ http/analysis-service/jobs/run.sh
@@ -10,10 +10,10 @@ if [ ! -f "$JOB_CONFIG" ]; then
     exit 1
 fi
 
-if [ $(jq 'has("num_workers")' < $JOB_CONFIG) = true ]; then # Spark cluster
+if [ ! -z "$(jq '.num_workers' < $JOB_CONFIG)" ]; then # Spark cluster
     AMI_VERSION=$(jq -r '.ami_version' < "$JOB_CONFIG")
     SPARK_VERSION=$(jq -r '.spark_version' < "$JOB_CONFIG")
-    N_WORKERS=$(jq -r '."num_workers"' < "$JOB_CONFIG")
+    N_WORKERS=$(jq -r '.num_workers' < "$JOB_CONFIG")
     MASTER_TYPE=$(jq -r '.master_instance_type' < "$JOB_CONFIG")
     SLAVE_TYPE=$(jq -r '.slave_instance_type' < "$JOB_CONFIG")
     CLUSTER_NAME=$(jq -r '.cluster_name' < "$JOB_CONFIG")
@@ -24,7 +24,7 @@ if [ $(jq 'has("num_workers")' < $JOB_CONFIG) = true ]; then # Spark cluster
     SSH_KEY=$(jq -r '.ssl_key_name' < "$JOB_CONFIG")
     OWNER=$(jq -r '.owner' < "$JOB_CONFIG")
 
-    aws emr create-cluster --auto-terminate --name $CLUSTER_NAME --ami-version $AMI_VERSION --instance-type $SLAVE_TYPE --instance-count $N_WORKERS --service-role EMR_DefaultRole --ec2-attributes KeyName=$SSH_KEY,InstanceProfile=telemetry-spark-emr --tags "Owner=$OWNER Application=telemetry-server" --bootstrap-actions Path=s3://support.elasticmapreduce/spark/install-spark,Args=\["-v","$SPARK_VERSION"\] Path=s3://telemetry-spark-emr/telemetry.sh,Args=\["--timeout","$TIMEOUT"\] --steps Type=CUSTOM_JAR,Name=CustomJAR,ActionOnFailure=TERMINATE_JOB_FLOW,Jar=s3://us-west-2.elasticmapreduce/libs/script-runner/script-runner.jar,Args=\["s3://telemetry-spark-emr/batch.sh","--job-name","$JOB_NAME","--notebook","$NOTEBOOK","--data-bucket","$DATA_BUCKET"\]
+    aws emr create-cluster --auto-terminate --name "$CLUSTER_NAME" --ami-version $AMI_VERSION --instance-type $SLAVE_TYPE --instance-count $N_WORKERS --service-role EMR_DefaultRole --ec2-attributes KeyName=$SSH_KEY,InstanceProfile=telemetry-spark-emr --tags "Owner=$OWNER Application=telemetry-server" --bootstrap-actions Path=s3://support.elasticmapreduce/spark/install-spark,Args=\["-v","$SPARK_VERSION"\] Path=s3://telemetry-spark-emr/telemetry.sh,Args=\["--timeout","$TIMEOUT"\] --steps Type=CUSTOM_JAR,Name=CustomJAR,ActionOnFailure=TERMINATE_JOB_FLOW,Jar=s3://us-west-2.elasticmapreduce/libs/script-runner/script-runner.jar,Args=\["s3://telemetry-spark-emr/batch.sh","--job-name","$JOB_NAME","--notebook","$NOTEBOOK","--data-bucket","$DATA_BUCKET"\]
 else
     cd "$DIR/../../../"
     python -m provisioning.aws.launch_worker "$JOB_CONFIG"
