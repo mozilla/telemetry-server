bcc73c2130ee41d3c9e369188b159fe17765dcd5
Vladan Djeric <vdjeric@mozilla.com>
2015-05-25 12:04:06 -0400

    Apparently there are machines reporting Windows 98 as well, this should be the last fix for these quirks

--- telemetry/convert.py
+++ telemetry/convert.py
@@ -221,7 +221,9 @@ class Converter:
 
         # WINNT is reported as Windows_NT in the unified ping, and apparently
         # we have some Win95 users
-        if info.get("OS") == "Windows_NT" or info.get("OS") == "Windows_95":
+        if info.get("OS") == "Windows_NT" or \
+           info.get("OS") == "Windows_95" or \
+           info.get("OS") == "Windows_98":
             info["OS"] = "WINNT"
 
         adapters = None
