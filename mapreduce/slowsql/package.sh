1c998bead64b3faa431c6532c1fb38ba34eccb97
Mark Reid <mreid@mozilla.com>
2015-10-23 12:05:46 -0400

    Don't upload the package to S3 anymore.

--- mapreduce/slowsql/package.sh
+++ mapreduce/slowsql/package.sh
@@ -19,12 +19,4 @@ tar czvf "$TARBALL" \
         run.sh \
         slowsql.py
 
-S3PATH=s3://telemetry-analysis-code/$NAME/$TARBALL
-
 echo "Packaged $NAME code as $TARBALL"
-if [ ! -z "$(which aws)" ]; then
-    aws s3 cp $TARBALL $S3PATH
-    echo "Code successfully uploaded to S3"
-else
-    echo "AWS CLI not found - you should manually upload to $S3PATH"
-fi
