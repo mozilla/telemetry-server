e9f8caa9632302cd8047fffd0a7cbc39927ff28a
Mark Reid <mreid@mozilla.com>
2015-11-25 10:27:13 -0400

    Add missing config values

--- http/analysis-service/config.py
+++ http/analysis-service/config.py
@@ -5,6 +5,8 @@ SECRET_KEY         = 'Overwrite with a secret on deployment'
 AWS_REGION         = 'us-west-2'
 INSTANCE_TYPE      = 'c3.4xlarge'
 WORKER_AMI         = 'ami-0057b733' # -> telemetry-worker-hvm-20151019 (Ubuntu 15.04)
+WORKER_PRIVATE_PROFILE = 'telemetry-example-profile'
+WORKER_PUBLIC_PROFILE  = 'telemetry-example-profile'
 
 # EMR configuration
 # Master and slave instance types should be the same as the telemetry
@@ -13,6 +15,8 @@ MASTER_INSTANCE_TYPE = INSTANCE_TYPE
 SLAVE_INSTANCE_TYPE = INSTANCE_TYPE
 SPARK_VERSION = '1.3.1.e'
 AMI_VERSION = '3.3.2'
+SPARK_INSTANCE_PROFILE = 'telemetry-example-profile'
+SPARK_EMR_BUCKET = 'example'
 
 # Make sure the ephemeral map matches the instance type above.
 EPHEMERAL_MAP      = { "/dev/xvdb": "ephemeral0", "/dev/xvdc": "ephemeral1" }
